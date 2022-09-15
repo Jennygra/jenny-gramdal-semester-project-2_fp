ea56387e42f210318d0e5ca77150e6f0bbcb6a9d
Jennygra <83270722+Jennygra@users.noreply.github.com>
2022-09-15 14:50:21 +0200

    Nav styling and product list

--- js/script.js
+++ js/script.js
@@ -3,12 +3,18 @@ import createNav from "./component/createNav.js";
 import { featureProduct } from "./component/featureProduct.js";
 import createNewsletter from "./component/newsletter.js";
 
+createNav();
+createNewsletter();
+
 const featureProductContainer = document.querySelector(
   ".feature-products-container"
 );
+const loaderContainer = document.querySelector(".loader--container");
 
-createNav();
-createNewsletter();
+setTimeout(() => {
+  loaderContainer.style.display = "none";
+  featureProductContainer.style.display = "";
+}, 1500);
 
 const productUrl = baseURL + "/products";
 
